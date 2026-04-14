2d9d3461d6b9fe4efdde2365042dd9d5bc351feb
jackssrt <33695699+jackssrt@users.noreply.github.com>
2026-04-15 01:29:16 +0300

    fix: debounce hot reload

--- src/hot_reloading.rs
+++ src/hot_reloading.rs
@@ -1,5 +1,7 @@
+use std::time::Duration;
+
 use anyhow::Context;
-use notify::Watcher;
+use notify_debouncer_full::DebounceEventResult;
 use tokio::sync::mpsc::Receiver;
 
 use crate::{args::Args, state::State};
@@ -22,21 +24,32 @@ pub fn setup(args: &'static Args) -> anyhow::Result<Receiver<()>> {
         args.config_path.display()
     );
 
-    let mut watcher =
-        notify::recommended_watcher(move |event: Result<notify::Event, notify::Error>| {
-            let file = event.expect("watcher error");
-            if file.paths.iter().any(|path| path == &args.config_path) && file.kind.is_modify() {
-                tracing::debug!("{:?}", file);
+    let mut debouncer = notify_debouncer_full::new_debouncer(
+        Duration::from_secs(2),
+        Some(Duration::from_secs(2)),
+        move |events: DebounceEventResult| {
+            tracing::trace!("file events: {:?}", events);
+            let events = events.expect("watcher error");
+            if events
+                .iter()
+                // text editors often do "atomic swaps", where they make a temp file and rename it to *swap* it in place
+                // imo this should just be built-in to the filesystem but whatever
+                .any(|e| e.paths.contains(&args.config_path) && !e.kind.is_access())
+            {
+                tracing::debug!("{:?}", events);
                 let _ = tx.try_send(());
             }
-        })?;
+        },
+    )?;
 
-    watcher.watch(
+    debouncer.watch(
         args.config_path
             .parent()
             .context("failed to get parent of config file")?,
         notify::RecursiveMode::NonRecursive,
     )?;
+    // the file watcher needs to be kept alive but we dont do anything with it after this, so just don't run its destructor
+    std::mem::forget(debouncer);
 
     Ok(rx)
 }
